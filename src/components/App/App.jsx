d3d520b9d25ad04d4908d9711d5bea0c4e96330c
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-09-02 21:39:22 -0500

    pictures added, GET Route working, pictures displaying on DOM. Error earlier was caused by VS coding importing Express at the top of App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+  let [galleryItems, setGalleryItems] = useState([]);
+
+  useEffect(() => {
+    console.log('useEffect - page load');
+    fetchPictures();
+  }, []);
+
+  const fetchPictures = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then(response => {
+      setGalleryItems(response.data);
+      console.log(response.data);
+    }).catch(error => {
+      console.log(error);
+      alert('something went wrong');
+    });
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <div>{galleryItems.map(picture => {
+
+        return <div key={picture.id} picutre={picture}/>
+
+        
+      })}</div>
+
+
+      <img src="images/atttower.jpg" />
+      <img src="images/bridge.jpg" />
+      <img src="images/chiles.jpg" />
+      <img src="images/clouds.jpg" />
+      <img src="images/foshay1.jpg" />
+      <img src="images/guthrie.jpg" />
+      <img src="images/guthrie2.jpg" />
+      <img src="images/uniondepot.jpg" />
+      <img src="images/weisman.jpg" />
+
+    </div>
+  );
 }
 
 export default App;
